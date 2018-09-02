b6201335661bf53b0cf71fe295375f5af5489950
John Doe <ajflory9@yahoo.com>
2018-09-02 13:13:29 -0400

    Added fortnite video and some styling

--- src/youtube.js
+++ src/youtube.js
@@ -5,7 +5,11 @@ class Youtube extends Component {
     render(){
         return(
             <div>
-                I am from YouTube
+                <button>Get Videos</button>
+                <div className="youtube">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/OlTi0MVHMFc" 
+                    frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+                </div>
             </div>
         )
     }
